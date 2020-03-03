e810ed60fa593fc1c8938d92fb5350884ae345b0
JordanTheJet <morepencils@gmail.com>
2020-03-03 08:53:37 -0600

    added main.tf output to backend

--- remote-backend/backend_setup.ps1
+++ remote-backend/backend_setup.ps1
@@ -4,5 +4,8 @@ $REGION=$args[1]
 
 $BUCKET_TERRAFORM = get-content .\bucket_template.txt
 
+get-content .\main.tf
+
 $BUCKET_TERRAFORM -replace 'default_project_name', "$PROJECT_NAME" -replace 'default_region', "$REGION"| out-file -encoding utf8 .\main.tf
 
+get-content .\main.tf
